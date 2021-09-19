8ec95822062a4341fe1927337176d9dd36810b2c
Anton Kudryavtsev <antonkudryavtsevdoem@gmail.com>
2021-09-19 19:05:24 +0300

    Format files

--- radix_string.py
+++ radix_string.py
@@ -1,40 +1,43 @@
 import string
 
-def radix_string(number:int, radix:int) -> str:
-   """Convert integer to radix string
 
-   Args:
-       number (int): a number to convert
-       radix (int): a bast convert to
+def radix_string(number: int, radix: int) -> str:
+    """Convert integer to radix string
 
-   Raises:
-       TypeError: number must be int
-       TypeError: radix must be int
-       ValueError: radix must be more than 2
+    Args:
+        number (int): a number to convert
+        radix (int): a bast convert to
 
-   Returns:
-       str: a radix string
-   """
-   if not isinstance(number, int):
-      raise TypeError("number must be int")
-   if not isinstance(radix, int):
-      raise TypeError("radix must be int")
-   if radix < 2:
-      raise ValueError("radix must be more than 2")
+    Raises:
+        TypeError: number must be int
+        TypeError: radix must be int
+        ValueError: radix must be more than 2
 
-   basic_alphabet = string.digits + string.ascii_lowercase
+    Returns:
+        str: a radix string
+    """
+    if not isinstance(number, int):
+        raise TypeError("number must be int")
+    if not isinstance(radix, int):
+        raise TypeError("radix must be int")
+    if radix < 2:
+        raise ValueError("radix must be more than 2")
 
-   result = ''
+    basic_alphabet = string.digits + string.ascii_lowercase
 
-   if number < 0:
-      number = -number
-      sign = '-'
-   else:
-      sign = ''
+    result = ""
 
-   while True:
-      number, rdigit = divmod( number, radix )
-      letter = f"[{rdigit}]" if rdigit >= len(basic_alphabet) else basic_alphabet[rdigit]
-      result = letter + result
-      if number == 0:
-         return sign + result
+    if number < 0:
+        number = -number
+        sign = "-"
+    else:
+        sign = ""
+
+    while True:
+        number, rdigit = divmod(number, radix)
+        letter = (
+            f"[{rdigit}]" if rdigit >= len(basic_alphabet) else basic_alphabet[rdigit]
+        )
+        result = letter + result
+        if number == 0:
+            return sign + result
